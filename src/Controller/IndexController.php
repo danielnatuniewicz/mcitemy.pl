41b49980929c4257d64747b059a8c2e3523a5033
danielnatuniewicz <77445591+danielnatuniewicz@users.noreply.github.com>
2024-03-05 21:24:50 +0100

    Update IndexController.php

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,9 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 
-//use App\Entity\Product;
-//use App\Entity\Category;
-
 class IndexController extends AbstractController
 {
     public function index(): Response
