dc151f546dda5f509673edc93e240d57cd0c5352
danielnatuniewicz <77445591+danielnatuniewicz@users.noreply.github.com>
2024-03-05 21:24:39 +0100

    Update IndexController.php

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,45 +11,8 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class IndexController extends AbstractController
 {
-    public function index(EntityManagerInterface $doctrine): Response
+    public function index(): Response
     {
-//        $product    = $doctrine->getRepository(Product::class)->productList();
-//        $category   = $doctrine->getRepository(Category::class)->categoryList();
-        // $product = new Product();
-        // $product->setName('tessst2');
-        // $product->setDescription('dssragon2');
-        // $product->setAvailable(120);
-        // $product->setPrice(2220);
-        // $product->setCategory(2);
-        // $product->setStatus(1);
-        // $entityManager->persist($product);
-        // $entityManager->flush();
-
-
-
-        // $product = new Category();
-        // $product->setName('dragonsurvival2.eu');
-        // $product->setEdition('2');
-        // $doctrine->persist($product);
-        // $doctrine->flush();        
-
-        /* kiedyś może będzie najpopularniejsze produkty z kategorią
-        // $sorted = [];
-        
-        // foreach($category as $ckey => $cvalue){
-        //     foreach($product as $pkey => $pvalue)
-        //         if($cvalue['id'] == $pvalue['category']){
-        //             $sorted[$cvalue['name']][] = ["name" => $pvalue['name'], "available" => $pvalue['available'], "price" => $pvalue['price']]; 
-        //         }
-        // }
-         //var_dump($sorted);
-        // return new Response('okay');
-        */
-
-        return $this->render('index/index.html.twig', [
-//            'category'  => $category,
-//            'product'   => $product,
-            //'sorted'    => $sorted
-        ]);
+        return $this->render('index/index.html.twig');
     }
 }
